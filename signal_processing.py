ebc33af05587eb4566320b4d7322d36912f2ba81
o2zheng <o2zheng@uwaterloo.ca>
2022-11-13 17:23:44 -0500

    function for bandpass filter and low pass filter

--- signal_processing.py
+++ signal_processing.py
@@ -30,3 +30,32 @@ trial2_stop_time = 67.61
 
 (EMG_data_1, time_array_1, sampling_rate_1) = clean_data(trial1_data_path, trial1_stop_time)
 (EMG_data_2, time_array_2, sampling_rate_2) = clean_data(trial2_data_path, trial2_stop_time)
+
+def filter_data(emg_data, sampling_rate, order):
+    nyquist_frequency = sampling_rate/2
+    
+    # bandpass filter to clean data
+    high = 20/500
+    low = 450/500
+    b, a = sp.signal.butter(order, [high,low], btype='bandpass')
+
+    emg_filtered = sp.signal.filtfilt(b, a, emg_data, axis=0)
+
+    emg_rectified = np.abs(emg_filtered)
+    
+    # lowpass filter to obtain the envelope
+    low_pass = 0.5/nyquist_frequency
+    b2, a2 = sp.signal.butter(order, low_pass, btype='lowpass')
+
+    emg_envelope = sp.signal.filtfilt(b2, a2, emg_rectified, axis=0)
+
+    return emg_envelope
+
+emg_data_envelope = filter_data(EMG_data_2, sampling_rate_2, 4)
+
+plt.figure()
+plt.plot(time_array_2, emg_data_envelope)
+plt.xlabel('Time (s)')
+plt.ylabel('EMG Signal')
+plt.title('Filtered EMG Data')
+plt.show()
