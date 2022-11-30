635ffc16856f8c1f7ea616595a855cd794ac81a4
Jacqueline L <o.jacqueline.lee@gmail.com>
2022-11-29 19:05:00 -0500

    clean up code and new data

--- signal_processing.py
+++ signal_processing.py
@@ -4,11 +4,15 @@ import scipy as sp
 from scipy import signal 
 
 
-def clean_data(data_path, stop_time):
-    EMG_data = np.loadtxt(data_path, dtype = int)
-    EMG_data = np.abs(np.diff(EMG_data))
-    data_length = EMG_data.size
-    time_array = np.linspace(0, stop_time, num = data_length)
+def clean_data(data_path, time_path):
+    emg_data = np.loadtxt(data_path, dtype=int)
+    emg_data = np.abs(np.diff(emg_data))
+    data_length = emg_data.size
+
+    contraction_times = np.loadtxt(time_path, dtype=float)
+    stop_time = contraction_times[-1]
+    time_data = np.linspace(0, stop_time, num = data_length)
+
     sampling_rate = np.reciprocal(np.divide(stop_time, data_length))
     
     '''
@@ -16,46 +20,44 @@ def clean_data(data_path, stop_time):
     plt.plot(time_array, EMG_data)
     plt.xlabel("Time (s)")
     plt.ylabel("Voltage (mV)")
-    plt.title("EMG Data")
+    plt.title("Raw EMG Data")
     plt.show()
     '''
 
-    return EMG_data, time_array, sampling_rate
-
-trial1_data_path = "./Arduino EMG Data/trial1.txt"
-trial1_stop_time = 82.11
+    return emg_data, contraction_times, time_data, sampling_rate
 
 trial2_data_path = "./Arduino EMG Data/trial2.txt"
-trial2_stop_time = 67.61
+trial2_time_path = "./Arduino EMG Data/trial2_contraction_times.txt"
 
-(EMG_data_1, time_array_1, sampling_rate_1) = clean_data(trial1_data_path, trial1_stop_time)
-(EMG_data_2, time_array_2, sampling_rate_2) = clean_data(trial2_data_path, trial2_stop_time)
+emg_data_raw, contraction_time_array, time_array, samp_rate = clean_data(trial2_data_path, trial2_time_path)
 
 def filter_data(emg_data, sampling_rate, order):
     nyquist_frequency = sampling_rate/2
-    
-    # bandpass filter to clean data
-    high = 20/500
-    low = 450/500
-    b, a = sp.signal.butter(order, [high,low], btype='bandpass')
 
-    emg_filtered = sp.signal.filtfilt(b, a, emg_data, axis=0)
+    # Bandpass filter to clean data
+    high_cutoff_band = 0.75/nyquist_frequency
+    low_cutoff_band = 15/nyquist_frequency
+    b_band, a_band = sp.signal.butter(order, [high_cutoff_band,low_cutoff_band], btype='bandpass')
+
+    emg_filtered = sp.signal.filtfilt(b_band, a_band, emg_data, axis=0)
 
     emg_rectified = np.abs(emg_filtered)
     
-    # lowpass filter to obtain the envelope
-    low_pass = 0.5/nyquist_frequency
-    b2, a2 = sp.signal.butter(order, low_pass, btype='lowpass')
+    # Lowpass filter to obtain the envelope
+    low_cutoff_lowpass = 0.5/nyquist_frequency
+    b_low, a_low = sp.signal.butter(order, low_cutoff_lowpass, btype='lowpass')
 
-    emg_envelope = sp.signal.filtfilt(b2, a2, emg_rectified, axis=0)
+    emg_envelope = sp.signal.filtfilt(b_low, a_low, emg_rectified, axis=0)
 
     return emg_envelope
 
-emg_data_envelope = filter_data(EMG_data_2, sampling_rate_2, 4)
+emg_data_envelope = filter_data(emg_data_raw, samp_rate, 4)
 
 plt.figure()
-plt.plot(time_array_2, emg_data_envelope)
+plt.plot(time_array, emg_data_envelope)
 plt.xlabel('Time (s)')
 plt.ylabel('EMG Signal')
 plt.title('Filtered EMG Data')
 plt.show()
+
+#hstack with time array
